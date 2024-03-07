cb325e3124b1ea212279bda7bd9ea1e9f97e18d0
aldopassossp <aldopassos@gmail.com>
2024-03-07 16:48:57 -0300

    Primeiro projeto spring

---
+++ wrapper/Wrapper.java
@@ -0,0 +1,15 @@
+package wrapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1.0f;
+        boolean tipoBoolean = false;
+
+        Float tipoFloat2 = 1F;
+        Boolean tipBoolean2 = false;
+        Integer tipoInteger = 1;
+
+        
+    }
+}
