177df667736caa84249a84a1080dea0b5d58f2c2
aldopassossp <aldopassos@gmail.com>
2024-03-15 19:46:51 -0300

    Entidade, controler e service de JOB

--- gestao_vagas/src/main/java/br/com/aldopassos/gestao_vagas/modules/company/entities/JobEntity.java
+++ gestao_vagas/src/main/java/br/com/aldopassos/gestao_vagas/modules/company/entities/JobEntity.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity(name = "job")
@@ -23,11 +24,12 @@ public class JobEntity {
     private UUID id;
     private String description;
     private String benefits;
+    @NotBlank(message = "Esse campo é obrigatório")
     private String level;
     @ManyToOne()
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
-    @Column(name = "company_id")
+    @Column(name = "company_id, nullable = false")
     private UUID companyId;
     @CreationTimestamp
     private LocalDateTime createdAt;
---
+++ gestao_vagas/src/main/java/br/com/aldopassos/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,12 @@
+package br.com.aldopassos.gestao_vagas.modules.company.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.aldopassos.gestao_vagas.modules.company.entities.JobEntity;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface JobRepository extends JpaRepository<JobEntity,UUID>{
+    
+}
